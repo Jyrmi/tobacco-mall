3bed5544b19948aa1fa009fbae0e7b4b7e7be9f0
Jeremy Choi <jeremyschoi@gmail.com>
2015-01-07 23:04:45 -0500

    installed bootstrap and introduced template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TobaccoMall::Application.config.secret_key_base = '64d39443b5c0fe99d93d3bb7a91121a2a65879a1378fea2e8ecd6be154d30cfd3c922c0c1bb4af2b174be3db999c5a9f28ec704b45cdef605281b9552b8368fb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TobaccoMall::Application.config.session_store :cookie_store, key: '_tobacco-mall_session'
